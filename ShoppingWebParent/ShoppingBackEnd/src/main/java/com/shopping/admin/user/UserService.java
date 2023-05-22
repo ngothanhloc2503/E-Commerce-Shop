8d1ec19909f85a1d854eb91e33f7f251b54f24ae
NTL2503 <ngoloc2706@gmail.com>
2023-05-22 23:26:11 +0700

    Add Encode Password

--- ShoppingWebParent/ShoppingBackEnd/src/main/java/com/shopping/admin/user/UserService.java
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/com/shopping/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shopping.admin.user;
 import com.shopping.common.entity.Role;
 import com.shopping.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +14,11 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired RoleRepository roleRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public List<User> listUser() {
         return (List<User>) userRepository.findAll();
@@ -24,6 +29,12 @@ public class UserService {
     }
 
     public void save(User user) {
+        encodePassword(user);
         userRepository.save(user);
+    }
+
+    private void encodePassword(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
