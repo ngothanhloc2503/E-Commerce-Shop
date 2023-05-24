6e8fca090dba182adbea64e2060a8b862c1df75e
NTL2503 <ngoloc2706@gmail.com>
2023-05-24 12:24:16 +0700

    Add Feature Photo Upload in User

--- ShoppingWebParent/ShoppingBackEnd/src/main/java/com/shopping/admin/user/UserController.java
+++ ShoppingWebParent/ShoppingBackEnd/src/main/java/com/shopping/admin/user/UserController.java
@@ -1,16 +1,17 @@
 package com.shopping.admin.user;
 
+import com.shopping.admin.FileUploadUtil;
 import com.shopping.common.entity.Role;
 import com.shopping.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.util.StringUtils;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -38,9 +39,26 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user, RedirectAttributes redirectAttributes) {
-        service.save(user);
+    public String saveUser(User user, RedirectAttributes redirectAttributes,
+                           @RequestParam(name = "image") MultipartFile multipartFile) throws IOException {
+
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setPhotos(fileName);
+
+            User savedUser = service.save(user);
+
+            String uploadDir = "user-photos/" + savedUser.getId();
+
+            FileUploadUtil.cleanDir(uploadDir);
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        } else {
+            if (user.getPhotos().isEmpty()) user.setPhotos(null);
+            service.save(user);
+        }
+
         redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+
         return "redirect:/users";
     }
 
