031d19bdec0525dcd0dcd4679ab1716738642c6a
NTL2503 <ngoloc2706@gmail.com>
2023-05-25 21:08:08 +0700

    Update for mobile

--- ShoppingCommon/src/main/java/com/shopping/common/entity/User.java
+++ ShoppingCommon/src/main/java/com/shopping/common/entity/User.java
@@ -133,4 +133,9 @@ public class User {
 
         return "/user-photos/" + this.id + "/" + this.photos;
     }
+
+    @Transient
+    public String getFullName() {
+        return this.firstname + " " + this.lastname;
+    }
 }
